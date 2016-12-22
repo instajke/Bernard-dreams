1a90f4bf275d9ea41eeb3e453b03ae462666228b
orineyl <orineyl@gmail.com>
2016-12-23 00:06:23 +0300

    env.port instead of env.app_port

--- server/config/production.js
+++ server/config/production.js
@@ -1,5 +1,5 @@
 module.exports = {
-  APP_PORT: process.env.APP_PORT || 3002,
+  APP_PORT: process.env.PORT || 3002,
   DB_HOST: process.env.DB_HOST || 'mongodb',
   DB_PORT: process.env.DB_PORT || 27017,
   DB_NAME: process.env.DB_NAME || 'klemanpromos-db'
