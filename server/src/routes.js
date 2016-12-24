34ddd0f88fa5b064a8d5bbf70e7e7b0409adbdb2
orineyl <orineyl@gmail.com>
2016-12-24 19:04:09 +0300

    deploy stuff

--- server/src/routes.js
+++ server/src/routes.js
@@ -538,8 +538,6 @@ router.post('/api/register', function(req, res) {
 
     // catch 404 and forward to error handler
     router.use(function(req, res, next) {
-        console.log(req);
-        console.log(res);
         var err = new Error('Not Found');
         err.status = 404;
         next(err);
