70b1d536d63436ea866b19ba92a28c207cd2713a
orineyl <orineyl@gmail.com>
2016-12-24 22:35:55 +0300

    deploy stuff

--- server/src/auth.js
+++ server/src/auth.js
@@ -4,19 +4,19 @@ module.exports = {
     'facebookAuth' : {
         'clientID'        : '1375969802415495', // your App ID
         'clientSecret'    : '891eb8085a8a4ef2584cda54909eb803', // your App Secret
-        'callbackURL'     : 'http://localhost:3000/api/facebook/callback'
+        'callbackURL'     : 'http://kmpm.eu-gb.mybluemix.net/api/facebook/callback'
     },
 
     'twitterAuth' : {
         'consumerKey'        : 'iELCH5VFlzUNW7pY2RGQBBrdS',
         'consumerSecret'     : 'wPhzbAiYRKjfAq2WoGrDEKTvv29lonEEMB6mMY7HZIS7UvWSZV',
-        'callbackURL'        : 'http://localhost:3000/api/twitter/callback'
+        'callbackURL'        : 'http://kmpm.eu-gb.mybluemix.net/api/twitter/callback'
     },
 
     'googleAuth' : {
         'clientID'         : '79570659241-ib42t4geja7cp1vsi5pigjo1934usnsh.apps.googleusercontent.com',
         'clientSecret'     : 'KNFNsi2OmWsnVYXE76RwQ83K',
-        'callbackURL'      : 'http://localhost:3000/api/google/callback'
+        'callbackURL'      : 'http://kmpm.eu-gb.mybluemix.net/api/google/callback'
     }
 
 };
